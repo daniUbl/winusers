46820b315b76efab173b007b1461509be935b157
Dani Brito <danielly.eng@hotmail.com>
2024-08-15 15:43:46 -0300

    add agents WinServ 2008, 2012R2, 2016 e melhorando o agente para o WinServ 2019

---
+++ agent/winusers-v1_0.ps1
@@ -0,0 +1,209 @@
+#===========================================================================
+# OCS Inventory Reports
+# Plugin WinUsers 
+# version 1.0 - 14/08/2024
+#
+# modify by Umbler.com
+#===========================================================================
+
+# Function to get Admin user status
+function Get-AdminUser {
+	param([string] $username)
+	$admingroup = Get-LocalGroupMember -SID "S-1-5-32-544" -ErrorAction SilentlyContinue
+	$userType = "Local"
+	
+	foreach ($admin in $admingroup) {
+		$name = $admin.name -split "\\"
+		if($name[1] -eq $username){
+			$userType = "Admin"
+		}
+	}
+	
+	return $userType
+}
+
+# Function to get user folder size
+function Get-Size
+{
+	param([string]$pth)
+	try {
+		"{0:n2}" -f ((gci -path $pth -recurse -ErrorAction Ignore | measure-object -ErrorAction Stop -property length -sum).sum /1mb)
+	} catch {
+		"{0:n2}" -f 0
+	}
+}
+
+# Function to check if is an AD user
+function Check-AdUser($username) { 
+    $ad_User = $null 
+	try {
+		$ad_User = Get-ADUser -Identity $username
+		return "Domain" 
+	} catch {
+		return "Unknown" 
+	}
+}
+
+# Function to retrieve user AD SID
+function Get-AdSid
+{
+	param([string]$pth, [array]$profileList)
+	foreach($sid in $profileList) {
+		if($pth -eq $sid.ProfileImagePath) {
+			return $sid.PSChildName
+		}
+	}
+
+	return ""
+}
+
+
+# Check if user belongs to group do IIS (IIS_IUSRS)
+function Check-User-IIS {
+	param([string] $username)
+
+	$iisgroup = net localgroup IIS_IUSRS
+	$descriptionCheck = ""
+
+	foreach ($member in $iisgroup){
+		if ($member.Trim() -eq $username) {
+            $descriptionCheck = "`n`n [* Usuario Membro do Grupo: IIS_IUSRS *]"
+            break
+        }
+	}
+
+	return $descriptionCheck
+}
+
+
+# Check if user belongs to group do "Remote Desktop Users"
+function Check-User-Remote {
+	param([string] $username)
+
+	$remotegroup = net localgroup "Remote Desktop Users"
+	$descriptionCheck = ""
+
+	foreach ($member in $remotegroup){
+		if ($member.Trim() -eq $username) {
+            $descriptionCheck = "`n`n [* Usuario Membro do Grupo: Remote Desktop Users *]"
+            break
+        }
+	}
+
+	return $descriptionCheck
+}
+
+
+#################################
+#          Local User           #
+#################################
+$users = Get-LocalUser | Select *
+$pathUsers = "C:\Users"
+$allUsers = @()
+
+$startTime = (get-date).AddDays(-15)
+$logEvents = Get-Eventlog -LogName Security -after $startTime | where {$_.eventID -eq 4624}
+
+foreach ($user in $users) {
+	if($user.Name -ne $null){
+	
+		$userType = Get-AdminUser $user.Name
+		$path = "C:\Users\"+ $user.Name
+		$folderSize = Get-Size $path
+		if($user.Enabled -ne "False") { $userStatus = "Disabled" } else { $userStatus = "Enabled" }
+		if($userType -eq "Local") { $userType = $user.PrincipalSource }
+
+		$numberConnexion = 0
+		$workstation = ""
+		$numberRemoteConnexion = 0
+		$ipRemote ="" 
+
+		foreach($userconnection in $logEvents){
+			#In local logon
+			if(($userconnection.ReplacementStrings[5] -eq $user.Name) -and (($userconnection.ReplacementStrings[8] -eq 2) -or ($userconnection.ReplacementStrings[8] -eq 7))){
+				$numberConnexion = $numberConnexion + 1
+				$workstation = $userconnection.ReplacementStrings[11]
+			#In remote
+			}if (($userconnection.ReplacementStrings[5] -eq $user.Name ) -and ($userconnection.ReplacementStrings[8] -eq 10)){
+				$workstation = $userconnection.ReplacementStrings[11]
+				$numberRemoteConnexion = $numberRemoteConnexion + 1
+				$ipRemote = $userconnection.ReplacementStrings[18]
+			}
+		}
+		
+        # verificar se o usuário está no grupo do IIS:
+		$descriptionUser = Check-User-IIS $user.Name
+		$user.Description += $descriptionUser
+
+		# verificar se usuário está no grupo do "Remote Desktop Users"
+		$descriptionUser = Check-User-Remote $user.Name
+		$user.Description += $descriptionUser
+
+
+		$xml += "<WINUSERS>`n"
+		$xml += "<NAME>"+ $user.Name +"</NAME>`n"
+		$xml += "<TYPE>"+ $userType +"</TYPE>`n"
+		$xml += "<SIZE>"+ $folderSize +"</SIZE>`n"
+		$xml += "<LASTLOGON>"+ $user.LastLogon +"</LASTLOGON>`n"
+		$xml += "<DESCRIPTION>"+ $user.Description +"</DESCRIPTION>`n"
+		$xml += "<STATUS>"+ $userStatus +"</STATUS>`n"
+		$xml += "<USERMAYCHANGEPWD>"+ $user.UserMayChangePassword +"</USERMAYCHANGEPWD>`n"
+		$xml += "<PASSWORDEXPIRES>"+ $user.PasswordExpires +"</PASSWORDEXPIRES>`n"
+		$xml += "<SID>"+ $user.SID +"</SID>`n"
+		$xml += "<USERCONNECTION>"+ $numberConnexion +"</USERCONNECTION>`n"
+		$xml += "<NUMBERREMOTECONNECTION>"+ $numberRemoteConnexion +"</NUMBERREMOTECONNECTION>`n"
+		$xml += "<IPREMOTE>"+ $ipRemote +"</IPREMOTE>`n"
+		$xml += "</WINUSERS>`n"
+
+		$allUsers += $user.Name
+	}
+}
+
+#################################
+#            AD User            #
+#################################
+# Get computer account type connection
+$Dsregcmd = New-Object PSObject ; Dsregcmd /status | Where {$_ -match ' : '} | ForEach { $Item = $_.Trim() -split '\s:\s'; $Dsregcmd | Add-Member -MemberType NoteProperty -Name $($Item[0] -replace '[:\s]','') -Value $Item[1] -EA SilentlyContinue }
+
+$profileListPath =  @("Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*")
+$profileList = Get-ItemProperty -Path $profileListPath -ErrorAction Ignore | Select ProfileImagePath, PSChildName
+
+$tmp = Get-ChildItem -Path $pathUsers | Select "Name"
+[System.Collections.ArrayList]$usersFolder = $tmp.Name
+
+while ($usersFolder -contains "Public") {
+	$usersFolder.Remove("Public")
+}
+
+$usersAd = $usersFolder | Where-Object {$allUsers -notcontains $_}
+
+foreach ($userAd in $usersAd) {
+	$path = "C:\Users\"+ $userAd
+
+	$sid = Get-AdSid $path $profileList
+
+	if($Dsregcmd.AzureAdJoined -eq "YES") {
+		$folderSize = Get-Size $path
+		$type = "AzureAD"
+	}
+
+	if($Dsregcmd.DomainJoined -eq "YES") {
+		if (Get-Command Get-ADUser -errorAction SilentlyContinue) {
+			$type = Check-AdUser -username $userAd
+			$folderSize = Get-Size $path
+		} else {
+			$type = "Domain"
+			$folderSize = Get-Size $path
+		}
+	}
+	
+	$xml += "<WINUSERS>`n"
+	$xml += "<NAME>"+ $userAd +"</NAME>`n"
+	$xml += "<TYPE>"+ $type +"</TYPE>`n"
+	$xml += "<SIZE>"+ $folderSize +"</SIZE>`n"
+	$xml += "<SID>"+ $sid +"</SID>`n"
+	$xml += "</WINUSERS>`n"
+}
+
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+[Console]::WriteLine($xml)
---
+++ agent/winusersWOlds-v1_0.ps1
@@ -0,0 +1,143 @@
+﻿#===========================================================================
+# OCS Inventory Reports - For Versions: Windows Server 2008, 2012 R2 and 2016
+# Plugin WinUsers 
+# version 1.0 - 14/08/2024
+#
+# modify by Umbler.com
+#===========================================================================
+
+# Function to get Admin user status
+function Get-AdminUser {
+    param([string] $username)
+
+    $admingroup = net localgroup Administrators
+    $userType = "Local"
+
+    foreach ($member in $admingroup) {
+        if ($member.Trim() -eq $username) {
+            $userType = "Admin"
+            break
+        }
+    }
+
+    return $userType
+}
+
+
+# Function to get user folder size
+function Get-Size
+{
+	param([string]$pth)
+	try {
+		"{0:n2}" -f ((gci -path $pth -recurse -ErrorAction Ignore | measure-object -ErrorAction Stop -property length -sum).sum /1mb)
+	} catch {
+		"{0:n2}" -f 0
+	}
+}
+
+
+# Check if user belongs to group do IIS (IIS_IUSRS)
+function Check-User-IIS {
+	param([string] $username)
+
+	$iisgroup = net localgroup IIS_IUSRS
+	$descriptionCheck = ""
+
+	foreach ($member in $iisgroup){
+		if ($member.Trim() -eq $username) {
+            $descriptionCheck = "`n`n[* Usuario Membro do Grupo: IIS_IUSRS *]"
+            break
+        }
+	}
+
+	return $descriptionCheck
+}
+
+
+# Check if user belongs to group do "Remote Desktop Users"
+function Check-User-Remote {
+	param([string] $username)
+
+	$remotegroup = net localgroup "Remote Desktop Users"
+	$descriptionCheck = ""
+
+	foreach ($member in $remotegroup){
+		if ($member.Trim() -eq $username) {
+            $descriptionCheck = "`n`n[* Usuario Membro do Grupo: Remote Desktop Users *]"
+            break
+        }
+	}
+
+	return $descriptionCheck
+}
+
+#################################
+#          Local User           #
+#################################
+$users = Get-WmiObject -Class Win32_UserAccount -Filter "LocalAccount=True" | Select *
+$pathUsers = "C:\Users"
+$allUsers = @()
+
+$startTime = (get-date).AddDays(-15)
+$logEvents = Get-Eventlog -LogName Security -After $startTime | where {$_.eventID -eq 4624}
+
+foreach ($user in $users) {
+	if($user.Name -ne $null){
+        $userDaVez = $user.Name
+
+
+        $userType = Get-AdminUser $user.Name
+        $path = "C:\Users\"+ $user.Name
+		$folderSize = Get-Size $path
+
+        if($user.Disabled) { $userStatus = "Disabled" } else { $userStatus = "Enabled" }
+
+        $numberConnexion = 0
+		$workstation = ""
+		$numberRemoteConnexion = 0
+		$ipRemote ="" 
+
+        foreach($userconnection in $logEvents){
+            #In local logon
+			if(($userconnection.ReplacementStrings[5] -eq $user.Name) -and (($userconnection.ReplacementStrings[8] -eq 2) -or ($userconnection.ReplacementStrings[8] -eq 7))){
+				$numberConnexion = $numberConnexion + 1
+				$workstation = $userconnection.ReplacementStrings[11]
+			#In remote
+			}if (($userconnection.ReplacementStrings[5] -eq $user.Name ) -and ($userconnection.ReplacementStrings[8] -eq 10)){
+				$workstation = $userconnection.ReplacementStrings[11]
+				$numberRemoteConnexion = $numberRemoteConnexion + 1
+				$ipRemote = $userconnection.ReplacementStrings[18]
+			}
+        }
+
+        # verificar se o usuário está no grupo do IIS:
+		$descriptionUser = Check-User-IIS $user.Name
+		$user.Description += $descriptionUser
+
+		# verificar se usuário está no grupo do "Remote Desktop Users"
+		$descriptionUser = Check-User-Remote $user.Name
+		$user.Description += $descriptionUser
+
+        
+		$xml += "<WINUSERS>`n"
+		$xml += "<NAME>"+ $user.Name +"</NAME>`n"
+		$xml += "<TYPE>"+ $userType +"</TYPE>`n"
+		$xml += "<SIZE>"+ $folderSize +"</SIZE>`n"
+		$xml += "<LASTLOGON>"+ $user.LastLogon +"</LASTLOGON>`n"
+		$xml += "<DESCRIPTION>"+ $user.Description +"</DESCRIPTION>`n"
+		$xml += "<STATUS>"+ $userStatus +"</STATUS>`n"
+		$xml += "<USERMAYCHANGEPWD>"+ $user.UserMayChangePassword +"</USERMAYCHANGEPWD>`n"
+		$xml += "<PASSWORDEXPIRES>"+ $user.PasswordExpires +"</PASSWORDEXPIRES>`n"
+		$xml += "<SID>"+ $user.SID +"</SID>`n"
+		$xml += "<USERCONNECTION>"+ $numberConnexion +"</USERCONNECTION>`n"
+		$xml += "<NUMBERREMOTECONNECTION>"+ $numberRemoteConnexion +"</NUMBERREMOTECONNECTION>`n"
+		$xml += "<IPREMOTE>"+ $ipRemote +"</IPREMOTE>`n"
+		$xml += "</WINUSERS>`n"
+
+		$allUsers += $user.Name
+    }
+}
+
+
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+[Console]::WriteLine($xml)
